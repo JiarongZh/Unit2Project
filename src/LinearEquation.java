807b63ce6c7431879662115a391d3007ae0be1da
student <sample.bths.edu>
2022-10-25 13:21:58 -0400

    fracslope

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -38,6 +38,27 @@ public class LinearEquation {
         }
         return equation;
     }
+    public String fracSlope(){
+        String slope = "";
+        double xD = x2 - x1;
+        double yD = y2 - y1;
+        double lower = 0.0;
+        if (xD>yD){
+            lower = yD;
+        } else {
+            lower = xD;
+        }
+        if (yD%xD == 0){
+            slope += yD/xD;
+            return slope;
+        } else if (yD%xD != 0){
+            for (int i = 2; i<lower; i++){
+                double modY = yD%i;
+                double modX = xD%i;
+                if (yD%i == 0 && xD%i == 0)
+            }
+        }
+    }
     public String lineInfo(){
         String cord1 = ("The first cord is :(" + x1 + "," + y1 + ")");
         cord1 += ("\n The Second cord is :(" + x2 + "," + y2 + ")");
